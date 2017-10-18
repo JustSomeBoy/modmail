bc774e6f2471378e531a7dd66c280c620df83e5a
JustSomeBoy <32719463+JustSomeBoy@users.noreply.github.com>
2017-10-18 16:20:25 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -328,13 +328,13 @@ class Modmail(commands.Bot):
     async def on_member_join(self, member):
         
         channel = member.guild.get_channel(366731334174900238)
-        sst = discord.Embed(title="__**Welcome To The Official Nebulous Discord Server! 😊**__", description = "👏 You are user #", color=0x00FFFF)
+        sst = discord.Embed(description = "__**Welcome To The Official Nebulous Discord Server! 😊**__", color=0x00FFFF)
         sst.set_author(name=str(member), icon_url=member.avatar_url)
         sst.set_thumbnail(url=member.avatar_url)
+        sst.add_field(name='\u200b', value="You are user", guild.member_count", inline=False)
         sst.add_field(name='\u200b', value="__**For Support, Please Message Me!**__", inline=False)
         sst.set_footer(text="Please Read #rules Before Chatting. Have a Great Day! ⛄")
-        await channel.send(member.mention)
-        await channel.send(embed=sst)
+        await channel.send(member.mention, embed=sst)
         
         try:
             embed = discord.Embed(title="__**Welcome To The Official Nebulous Discord Server!**__", description="Please read our rules before chatting!", color=0xFF4500)
