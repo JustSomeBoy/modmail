0e478fda11a3d3f48d3167e8b2cfb27d13a3fe92
JustSomeBoy <32719463+JustSomeBoy@users.noreply.github.com>
2017-10-18 16:41:39 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -329,12 +329,11 @@ class Modmail(commands.Bot):
         
         channel = member.guild.get_channel(366731334174900238)
         count = member.guild.member_count
-        sst = discord.Embed(description = "__**Welcome To The Official Nebulous Discord Server! 😊**__", color=0x00FFFF)
+        sst = discord.Embed(description = "__**🎃 Welcome To The Official Nebulous Discord Server! 🎃**__", color=0x00FFFF)
         sst.set_author(name=str(member), icon_url=member.avatar_url)
         sst.set_thumbnail(url=member.avatar_url)
-        sst.add_field(name='\u200b', value=f"👏 You are user # **{count}** 👏", inline=False)
-        sst.add_field(name='\u200b', value="__For Support, Please Message Me!__", inline=False)
-        sst.set_footer(text="Please Read #rules Before Chatting. Have a Great Day! ⛄")
+        sst.add_field(name='\u200b', value=f"👏 You are user **# {count}** 👏", inline=False)
+        sst.set_footer(text="For Support, Please Message ME! Have a Great Day! ⛄")
         await channel.send(member.mention, embed=sst)
         
         try:
